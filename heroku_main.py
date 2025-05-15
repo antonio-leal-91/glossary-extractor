b8611222b2f82f8cd137642ab2ea875c82d78121
Antonio Leal <antonio.leal@globalingua.eu>
2025-05-15 14:24:46 +0200

    Fix heroku entrypoint

--- heroku_main.py
+++ heroku_main.py
@@ -1,10 +1,1 @@
-# heroku_main.py
 from app import app
-
-# Nota: Este archivo permite que Heroku arranque la app
-# No debe contener lógica de ejecución adicional
-def create_app():
-    return app
-
-if __name__ == "__main__":
-    app.run(debug=False)
